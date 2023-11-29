e86a5d3e86dd1298b1ec7456a89b25228a25ec43
iagoPinheiro1401 <iago.pinheirodeoliveira@gmail.com>
2023-11-28 21:41:44 -0300

    ccomeço do backend e criado validador

---
+++ pages/api/user/signup.js
@@ -0,0 +1,22 @@
+/* eslint-disable import/no-extraneous-dependencies */
+import connect from 'next-connect'
+import Joi from 'joi'
+
+import validate from '../../../lib/middleware/validation'
+import { signupUser } from '../../../modules/user/user.service'
+
+const postSchema = Joi.object({
+    nome: Joi.string().required().max(50),
+    email: Joi.string().required().email().max(150),
+    senha: Joi.string().required().max(20).min(6),
+    ddd: Joi.number().required().min(2),
+    telefone: Joi.number().required().min(8)
+})
+
+const signup = connect()
+   .post(validate({ body: postSchema }), (req, res) => {
+        signupUser()
+        res.status(200).json({ teste: 'ok'})
+   })
+
+export default signup
